3b3f6a61d2ccc7e0f63ac00a7c5b66e7073b76a4
Suraj Gupta <suraj520876@gmail.com>
2024-02-25 14:48:47 +0545

    Added comment in CheckoutTest

--- src/test/java/testCases/eCommerce/checkOut/CheckOutTest.java
+++ src/test/java/testCases/eCommerce/checkOut/CheckOutTest.java
@@ -10,7 +10,17 @@ import pageObjectModel.eCommerce.checkoutPage.CheckOutPage;
 
 public class CheckOutTest extends BaseClass {
     private CheckOutPage checkOutPage;
+//This test case include
+/*Test the checkout process with a guest account from start to finish,
+including filling in personal information, shipping address, selecting
+a shipping method, payment information (use a test payment
+method provided by the platform), and confirming the order.*/
 
+    /*Test the checkout process with a registered account and validate
+that the user's saved information is populated correctly*/
+
+    /*Validate error handling for missing or incorrect information in the
+checkout process.*/
 
     @Epic("Couch Cache")
     @Severity(SeverityLevel.NORMAL)
